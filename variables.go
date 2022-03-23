fe7936fef81c8508d7ba566c9b7d489000080f4e
Luis Mendez <dluismendezdj@gmail.com>
2022-03-23 10:14:02 -0400

    Variables

---
+++ variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const pi float64 = 3.14
+	const age int32 = 5
+
+	num1 := 5
+	var num2 int = 15
+	var result int
+
+	result = num1 + num2
+
+	fmt.Println(pi)
+	fmt.Println(result)
+}
